ef679c139d270c752696d6d8f2f130ba41ea3786
Alper Said Soylu <54alpersaid@gmail.com>
2019-01-29 18:50:47 +0300

    normalize_background

--- utils.py
+++ utils.py
@@ -9,7 +9,7 @@ def rgb2hex(color):
     return '#%02x%02x%02x' % color
 
 
-def normalize_brightness(rgb, s=0.9, v=0.8):
+def normalize_brightness(rgb, s=0.9, v=0.65):
     r, g, b = rgb
     hsv = [*colorsys.rgb_to_hsv(r/255, g/255, b/255)]
     hsv = hsv[0], s, v
